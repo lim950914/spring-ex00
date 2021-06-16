edfc10cdf0dd619ed3bc177f91f83ed751ab969a
lim950914 <dlackswn2222@naver.com>
2021-06-16 15:26:54 +0900

    20210616B

--- src/main/java/org/zerock/mapper/TimeMapper.java
+++ src/main/java/org/zerock/mapper/TimeMapper.java
@@ -1,7 +1,11 @@
 package org.zerock.mapper;
 
+import org.apache.ibatis.annotations.Select;
+
 public interface TimeMapper {
 	
 	@Select("SELECT now()")
 	public String getTime();
+	
+	public String getTime2();
 }
