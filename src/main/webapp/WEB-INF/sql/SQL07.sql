00761bad9d03d95355461915ee5baf6af1c154f6
lim950914 <dlackswn2222@naver.com>
2021-07-06 12:00:49 +0900

    20210706A

---
+++ src/main/webapp/WEB-INF/sql/SQL07.sql
@@ -0,0 +1,19 @@
+USE spr1;
+
+DESC tbl_board;
+
+CREATE TABLE tbl_board_file (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    bno INT REFERENCES tbl_board(bno),
+    fileName VARCHAR(200) NOT NULL
+);
+
+DESC tbl_board_file;
+
+DELETE FROM tbl_board_file;
+
+ALTER TABLE tbl_board_file
+ADD FOREIGN KEY (bno) REFERENCES tbl_board(bno);
+
+SELECT * FROM tbl_board_file;
+SELECT * FROM tbl_board;
